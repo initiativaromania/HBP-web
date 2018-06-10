06220fc1bb7441633df3ab58a225e7f12b1a6d0e
George Barbarosie <george.barbarosie@gmail.com>
2018-06-10 20:52:37 +0300

    funcioning server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const { Nuxt, Builder } = require('nuxt')
+
+const app = require('express')()
+const isProd = (process.env.NODE_ENV === 'production')
+const port = process.env.PORT || 3000
+
+// We instantiate nuxt.js with the options
+const config = require('./nuxt.config.js')
+config.dev = !isProd
+const nuxt = new Nuxt(config)
+
+// Render every route with Nuxt.js
+app.use(nuxt.render)
+
+// Build only in dev mode with hot-reloading
+if (config.dev) {
+  new Builder(nuxt).build()
+  .then(listen)
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
+}
+else {
+  listen()
+}
+
+function listen() {
+  // Listen the server
+  app.listen(port, '0.0.0.0')
+  console.log('Server listening on `localhost:' + port + '`.')
+}
